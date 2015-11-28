fc75991d7a600dbd15c3ea5bb2d554f99704d5a2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-28 18:49:18 -0500

    refactor(filtering): filtering by valid version

--- src/left-behind.js
+++ src/left-behind.js
@@ -5,6 +5,7 @@ var Promise = require('bluebird');
 var getPackageJson = require('get-package');
 var _ = require('lodash');
 var log = require('debug')('behind');
+var toExact = require('to-exact-semver');
 
 function getPackage(name) {
   return new Promise(function (resolve, reject) {
@@ -45,17 +46,22 @@ function leftBehind(options) {
   var reporter = require(reporterModule);
   la(check.fn(reporter), 'missing reporter', reporter, reporterModule);
 
+  var fetchOptions = {
+    concurrency: 5
+  };
+
   Promise.resolve(topDeps.topDependents(name, n))
     .then(function (list) {
-      console.log('this packages depend on %s', name);
-      console.log(list);
+      console.log('found %d packaged dependent on %s', list.length, name);
+      log('these packages depend on %s', name);
+      log(list);
       return list;
     })
     .then(function (list) {
       log('leaving just a sample of %d dependents out of %d', n, list.length);
       return _.sample(list, n);
     })
-    .map(getPackage)
+    .map(getPackage, fetchOptions)
     .then(function (list) {
       // list of package.json files
       return list.map(function findVersion(package) {
@@ -66,14 +72,31 @@ function leftBehind(options) {
       });
     })
     .tap(function (versions) {
-      console.log('package %s', name, 'is used by dependent projects');
+      console.log('package %s', name, 'is used by', versions.length, 'dependent projects');
       console.log(versions);
     })
     .then(function (versions) {
-      return getPackage(name).
-        then(function (pkg) {
-          return [name, pkg.version, versions];
-        });
+      return versions.map(function (info) {
+        var usesVersion = info.uses;
+        if (check.not.unemptyString(usesVersion)) {
+          return;
+        }
+        var exact = toExact(info.name, usesVersion);
+        if (check.unemptyString(exact)) {
+          return {
+            name: info.name,
+            uses: exact
+          };
+        }
+      }).filter(function hasVersion(info) {
+        return info && check.unemptyString(info.uses);
+      });
+    })
+    .then(function (versions) {
+      console.log('%d dependent projects after version filtering', versions.length);
+      return getPackage(name).then(function (pkg) {
+        return [name, pkg.version, versions];
+      });
     })
     .spread(reporter);
 }
