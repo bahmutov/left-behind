6534c52bc731454aa095b58b289e9b21fe6b70b2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-28 00:27:16 -0500

    fix(fuzzy): Handles fuzzy versions
    closes #7

--- src/reporter-bars.js
+++ src/reporter-bars.js
@@ -2,16 +2,22 @@ var la = require('lazy-ass');
 var check = require('check-more-types');
 var bars = require('bars');
 var sortByVersion = require('./sort-by-version');
+var toExactSemver = require('to-exact-semver');
 
 function reporter(versionInfo) {
   la(check.array(versionInfo));
   var data = {};
   versionInfo.forEach(function (info) {
     la(check.unemptyString(info.uses), info);
-    if (data[info.uses]) {
-      data[info.uses] += 1;
+    var exactVersion = toExactSemver(info.name, info.uses);
+    if (check.not.unemptyString(exactVersion)) {
+      return;
+    }
+
+    if (data[exactVersion]) {
+      data[exactVersion] += 1;
     } else {
-      data[info.uses] = 1;
+      data[exactVersion] = 1;
     }
   });
 
