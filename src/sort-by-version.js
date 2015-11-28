7bfab533a0aa36758f03fd9b6af7fb46738998cb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-28 00:01:45 -0500

    refactor(semver): semver comparison

--- src/sort-by-version.js
+++ src/sort-by-version.js
@@ -3,18 +3,14 @@ var check = require('check-more-types');
 var _ = require('lodash');
 var semver = require('semver');
 
-function compareVersions(a, b) {
-  var versionA = a[0];
-  var versionB = b[0];
-
-  if (semver.lt(versionA, versionB)) {
-    return -1;
-  } else if (semver.gt(versionA, versionB)) {
-    return 1;
-  } else {
-    return 0;
-  }
+function compareFirstArguments(fn) {
+  la(check.fn(fn), 'expected a function', fn);
+  return function (a, b) {
+    return fn(a[0], b[0]);
+  };
 }
+
+var compareVersions = compareFirstArguments(semver.compare);
 
 // TODO move to k2, make more general
 function sortByVersion(o, descending) {
