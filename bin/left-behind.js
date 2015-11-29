d59be18b4960c5535418c571af0f7750462fa06f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-29 13:30:28 -0500

    feat(help): Using simple-bin-help

--- bin/left-behind.js
+++ bin/left-behind.js
@@ -1,20 +1,10 @@
 #!/usr/bin/env node
 
-function noArguments() {
-  return process.argv.length < 3;
-}
-
-function showHelp() {
-  var join = require('path').join;
-  var pkg = require(join(__dirname, '..', 'package.json'));
-  console.log('%s@%s - %s', pkg.name, pkg.version, pkg.description);
-  console.log('use: %s <package name>', pkg.name);
-}
-
-if (noArguments()) {
-  showHelp();
-  process.exit(0);
-}
+require('simple-bin-help')({
+  minArguments: 3,
+  packagePath: __dirname + '/../package.json',
+  help: 'use: left-behind <package name>'
+});
 
 var name = process.argv[2];
 
